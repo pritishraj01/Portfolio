2109da42c85053f91d678d9c84d8f39704bbbfb9
pritishraj01 <pritish499@gmail.com>
2025-10-11 23:14:49 +0530

    initial commit

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -15,7 +15,7 @@ function Projects() {
       <h2>2+ YEARS EXPERIENCE IN PROJECTS</h2>
       <div className="slider">
         <a href='http://mychatgptai.netlify.app' target='blank' rel='noopener noreferrer'><Card title="CHATGPT" img={chatgpt} id="chatgpt" /></a>
-        <a href='https://pritishhportfolio.netlify.app/' target='blank' rel='noopener noreferrer'><Card title="WEATHER CONDITION" img={weather} id="weather" /></a>
+        <a href='https://linkedin-frontend-6grn.onrender.com' target='blank' rel='noopener noreferrer'><Card title="WEATHER CONDITION" img={weather} id="weather" /></a>
       </div>
     </div>
   )
