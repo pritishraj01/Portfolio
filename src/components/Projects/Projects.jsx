8482b8c6168df0c96fbe13a54b32adde673d88e8
pritishraj01 <pritish499@gmail.com>
2026-01-03 19:03:01 +0530

    adding project error detector

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import './Projects.css'
 import Card from '../Card/Card'
 import chatgpt from '../../assets/chatgpt.png'
 import linkedIn from '../../assets/linkedin.png'
-
+import error from "../../assets/error.png"
 
 
 function Projects() {
@@ -12,7 +12,7 @@ function Projects() {
       <h2>1+ YEARS EXPERIENCE IN PROJECTS</h2>
       <div className="slider">
         <a href='https://linkedin-frontend-6grn.onrender.com' target='blank' rel='noopener noreferrer'><Card title="LINKEDIN" img={linkedIn} id="linkedin" /></a>
-        <a href='https://gemini-5z3p.onrender.com' target='blank' rel='noopener noreferrer'><Card title="CHATGPT" img={chatgpt} id="chatgpt" /></a>
+        <a href='https://errordetector.onrender.com' target='blank' rel='noopener noreferrer'><Card title="Error Detector" img={error} id="chatgpt" /></a>
       </div>
     </div>
   )
